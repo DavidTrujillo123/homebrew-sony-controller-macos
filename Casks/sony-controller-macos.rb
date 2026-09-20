3f49332edfb1e947bb4c5d63d2d6cb7df981eb12
David Trujillo <davot098@gmail.com>
2026-09-20 13:21:23 -0500

    Translate Cask description to English

--- Casks/sony-controller-macos.rb
+++ Casks/sony-controller-macos.rb
@@ -4,7 +4,7 @@ cask "sony-controller-macos" do
 
   url "https://github.com/DavidTrujillo123/sony-controller-macos/releases/download/v0.1.0/SonyHeadphonesMac-0.1.0.zip"
   name "Sony Controller macOS"
-  desc "Controla ANC/EQ/batería/volumen de tus Sony WH-1000XM5 desde la barra de menú"
+  desc "Control ANC/EQ/battery/volume for your Sony WH-1000XM5 from the menu bar"
   homepage "https://github.com/DavidTrujillo123/sony-controller-macos"
 
   depends_on macos: :ventura
